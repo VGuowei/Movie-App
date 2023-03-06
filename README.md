9737b29e9aa8ef064d6e75eb4c001bdd8de4111c
VGuowei <basiliou.guowei@gmail.com>
2023-03-06 18:20:26 +0200

    new project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Movie Maniac
+
+The mobile computing coursework, a mobile application that use TheMovieDB API.
+
+
---
+++ android/app/src/main/kotlin/com/example/movie_maniac/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movie_maniac
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
